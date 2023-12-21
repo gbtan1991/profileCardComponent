f41de3ceb4c7ca0f2276fc3f77f5f07a8bc201af
Gilbert Tan <57788135+gbtan1991@users.noreply.github.com>
2023-12-21 14:54:20 +0800

    Add the assets and tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkCyan: "hsl(185, 75%, 39%)",
+        veryDarkDesaturatedBlue: "hsl(229, 23%, 23%)",
+        darkGrayishBlue: "hsl(227, 10%, 46%)",
+        darkGray: "hsl(0, 0%, 59%)",
+      },
+
+      fontFamily: {
+        theme: "Kumbh Sans, san-serif",
+      },
+    },
   },
   plugins: [],
-}
+};
